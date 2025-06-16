ed864e17f713bb30cd9c0703885961785aef8c1a
Joann-code <sjoanitana@gmail.com>
2025-06-16 23:48:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById('display');
+  
+  function appendValue(val) {
+    if (display.innerText === '0') display.innerText = '';
+    display.innerText += val;
+  }
+  
+  function clearDisplay() {
+    display.innerText = '0';
+  }
+  
+  function calculate() {
+    try {
+      display.innerText = eval(display.innerText);
+    } catch {
+      display.innerText = 'Error';
+    }
+  }
